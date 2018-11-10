99901c6a26023a95549fedfa8efdb10c86452836
Sreedevi <sreedevivp4u@gmail.com>
2018-11-10 15:09:47 +0000

    Add project files.

---
+++ MVCATM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCATM.Startup))]
+namespace MVCATM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
